117c3989ec831224f422d4850fa896bf7003358a
krisk <krisk@krisks-MacBook-Pro.local>
2024-07-03 01:17:57 -0700

    gravity pathing kinda working

--- addons/gravity-gizmo/gizmo.gd
+++ addons/gravity-gizmo/gizmo.gd
@@ -13,7 +13,11 @@ func _has_gizmo(node):
 func _redraw(gizmo):
 	gizmo.clear()
 	var node3d : RigidBody3D = gizmo.get_node_3d()
-	var lines = PackedVector3Array([Vector3(2, 1, 3), Vector3.ZERO])
+	var positions = node3d.get_meta("simpos")[1]
+	var id = node3d.get_meta("simpos")[0]
+	
 	var material : StandardMaterial3D = StandardMaterial3D.new()
 	material.albedo_color = Color.GREEN
-	gizmo.add_lines(lines, material, false)
+	for i in range(len(positions)-1):
+		print([positions[i].get_child(id).position - node3d.position, positions[i+1].get_child(id).position - node3d.position])
+		gizmo.add_lines(PackedVector3Array([positions[i].get_child(id).position - node3d.position, positions[i+1].get_child(id).position - node3d.position]), material, false)
--- world.gd
+++ world.gd
@@ -2,22 +2,20 @@
 extends Node3D
 
 var G = 0.1
-var bodies = []
-
 func _physics_process(delta):
 	if Engine.is_editor_hint():
-		#PhysicsServer3D.set_active(true)
-		#var planets = $planets.duplicate()
-		#for i in planets.get_children():
-			#i.set_meta("simpos", [])
-		#for i in range(10):
-			#for body : RigidBody3D in planets.get_children():
-				#for other_body : RigidBody3D in planets.get_children():
-					#if body == other_body:
-						#continue
-					#body.set_meta("simpos", body.get_meta("simpos") + [body.position])
-					#body.position += (other_body.position - body.position).normalized()*G*body.mass*other_body.mass/(other_body.position - body.position).length_squared()
-		#print(planets.get_child(0).get_meta("simpos"))
+		var positions = []
+		var planets = $planets.duplicate()
+		for i in range(20):
+			for body : RigidBody3D in planets.get_children():
+				for other_body : RigidBody3D in planets.get_children():
+					if body == other_body:
+						continue
+					body.linear_velocity += 0.1/body.mass*(other_body.position - body.position).normalized()*G*body.mass*other_body.mass/(other_body.position - body.position).length_squared()
+					body.position += body.linear_velocity*0.1
+			positions.append(planets.duplicate())
+		for i in range(len($planets.get_children())):
+			$planets.get_child(i).set_meta("simpos", [i, positions])
 	else:
 		for body : RigidBody3D in $planets.get_children():
 			for other_body : RigidBody3D in $planets.get_children():
